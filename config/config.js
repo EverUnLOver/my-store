d9db90b622add66dfe1d3f48f95997f6ad981044
Sebastian Herrera <sebastianherrera@Sebastians-MacBook-Air.local>
2022-07-01 12:23:16 -0500

    apply dbUrl

--- config/config.js
+++ config/config.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'development',
+  isProd: process.env.NODE_ENV === 'producttion',
   port: process.env.PORT || 3000,
   dbName: process.env.DB_NAME || 'my_store',
   dbHost: process.env.DB_HOST || 'localhost',
   dbPort: process.env.DB_PORT || 5432,
   dbUser: process.env.DB_USER || 'Sebas',
   dbPassword: process.env.DB_PASSWORD || 'admin32|',
+  dbUrl: process.env.DATABASE_URL,
 }
 
 module.exports = { config }
